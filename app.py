ac4dd11eee902b359a9fa29cde2d0f4b0d4d5532
Dmitry Drozdov <dinochrom1@gmail.com>
2023-02-11 18:15:34 +0400

    init

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.title("Hello World")
+
+def f1():
+    st.session_state['show_button'] = True
+
+
+def f2():
+    st.session_state['show_button'] = False
+
+
+st.write("Hello World!!!")
+
+if 'show_button' not in st.session_state:
+    st.session_state['show_button'] = True
+
+if st.session_state['show_button']:
+    st.button("Main Button", on_click=f2)
+
+st.button("Show Button", on_click=f1)
